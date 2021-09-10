b832a06938de6580433710f628f55b0ea1b1790e
Gare_TH <329288574@qq.com>
2021-09-10 18:52:59 -0400

    more progress on MySQLManager.java.

--- src/main/java/com/mystatus/application/database/MySQLManager.java
+++ src/main/java/com/mystatus/application/database/MySQLManager.java
@@ -7,6 +7,8 @@ import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -18,17 +20,16 @@ public class MySQLManager {
         // MySQL 8.0 +
         String driver = "com.mysql.cj.jdbc.Driver";
         DbUtils.loadDriver(driver);
-        ConfigHandler cf = ConfigHandler.getInstance();
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setURL(getDBUrl(cf));
-        dataSource.setUser(cf.getStringConfig("DB_Username"));
-        dataSource.setPassword(cf.getStringConfig("DB_Password"));
-        runner = new QueryRunner(dataSource);
+        runner = new QueryRunner(getDataSource());
     }
 
     public static MySQLManager getInstance(){
         if(instance == null) instance = new MySQLManager();
         return instance;
+    }
+
+    public void createQuery(String sql) throws SQLException {
+        runner.update(sql);
     }
 
     public<T> List<T> selectQuery(String sql, Class<T> tClass , Object... args) throws SQLException {
@@ -40,12 +41,53 @@ public class MySQLManager {
         return runner.update(sql, args);
     }
 
-    private String getDBUrl(ConfigHandler cf){
-        String localurl = cf.getStringConfig("DB_URL");
-        String port = cf.getStringConfig("DB_Port");
-        String dbname = cf.getStringConfig("DB_Name");
-        String params = cf.getStringConfig("DB_Params");
-        return "jdbc:mysql://" + localurl + ':' + port + '/' + dbname + '?' + params;
+    private MysqlDataSource getDataSource(){
+        ConfigHandler cf = ConfigHandler.getInstance();
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setServerName(cf.getStringConfig("DB_Server"));
+        dataSource.setDatabaseName(cf.getStringConfig("DB_Name"));
+        dataSource.setUser(cf.getStringConfig("DB_Username"));
+        dataSource.setPort(cf.getIntegerConfig("DB_Port"));
+        dataSource.setPassword(cf.getStringConfig("DB_Password"));
+        try {
+            dataSource.setCharacterEncoding("UTF-8");
+            dataSource.setServerTimezone("UTC");
+            dataSource.setRequireSSL(false);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
+
+    // Will be moved to other class.
+    private void createTable(){
+        if(tableExists("mystatusdb")) return;
+        String sql = "CREATE TABLE mystatusdb (" +
+                "  id INT NOT NULL AUTO_INCREMENT," +
+                "  item VARCHAR(45) NULL," +
+                "  count INT NULL," +
+                "  price DECIMAL(10, 2) NULL," +
+                "  player VARCHAR(45) NULL," +
+                "  type ENUM('buy', 'sell') NULL," +
+                "  owner VARCHAR(45) NULL," +
+                "  PRIMARY KEY (`id`));";
+        try{
+            createQuery(sql);
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean tableExists(String tableName) {
+        try{
+            DatabaseMetaData meta = runner.getDataSource().getConnection().getMetaData();
+            ResultSet resultSet = meta.getTables(null, null, tableName, new String[] {"TABLE"});
+            return resultSet.next();
+        }catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+
     }
 
 }
